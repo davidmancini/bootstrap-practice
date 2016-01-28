872fbb42417464fd3bd492b60765b2c4887dd3ac
David Mancini <mancini.david@gmail.com>
2016-01-28 08:24:52 -0700

    Initial git commit; GitHub was down, so I developed it locally.

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Bootstrap Wireframe Practice</title>
+		<?php //require_once(dirname(__DIR__) . "/bootstrap-practice/lib/head-tags.html"); ?>
+		<?php require_once("lib/head-tags.html"); ?>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="content">
+
+				<!-- HEADER -->
+				<div class="row headerbox">
+					<div class="col-md-6">
+						<img src="images/logo.png" alt="Penguin Logo">
+					</div>
+					<div class="col-md-6 text-right">
+						<button role="button" class="btn btn-default" href="#">Products</button>
+						<button role="button" class="btn btn-default" href="#">Prices</button>
+						<button role="button" class="btn btn-default" href="#">Contact Us</button>
+					</div>
+				</div>
+
+				<!-- CONTENT AREA -->
+				<div class="row content-area">
+					<div class="col-md-12">
+						<img src="images/content-guy.png" alt="Another Penguin" class="img-responsive pull-right">
+						<h1>Penguin, Inc.</h1>
+						<p>Our company is the best.<br>We have the most pengin clothing available on the web. Lorem ipsum
+							dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+							magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+							ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+							eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+							Our company is the best. We have the most pengin clothing available on the web. Lorem ipsum dolor
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+							commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</p>
+					</div>
+				</div>
+
+				<div class="row content-area">
+					<div class="col-md-6">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+						dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+						officia deserunt mollit anim id est laborum.
+					</div>
+					<div class="col-md-6">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+						dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+						officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+
+				<div class="row content-area">
+					<div class="col-md-3">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</div>
+					<div class="col-md-3">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</div>
+					<div class="col-md-3">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</div>
+					<div class="col-md-3">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</div>
+				</div>
+			</div>
+			<!-- FOOTER -->
+			<footer>
+				<div class="row footer">
+					<div class="col-md-12">
+						This site was brought to you by David Mancini.
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
